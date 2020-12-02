b39dba125c93d921eded62743058cee30bb2d2b5
Toni <black_jack_21@gmx.de>
2020-12-02 23:25:44 +0100

    add tons of stuff...

---
+++ prototyping103-notmygamedesign/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public GameObject playerExplosion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(playerExplosion, transform.position, transform.rotation);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
